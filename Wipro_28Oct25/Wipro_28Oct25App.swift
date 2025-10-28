4ed1938a80a6a8828b942b64c8ab68b8ba55a636
Prem Sai K Varma <premsaikvarma@outlook.com>
2025-10-28 21:05:51 +0530

    Initial Commit

---
+++ Wipro_28Oct25/Wipro_28Oct25App.swift
@@ -0,0 +1,17 @@
+//
+//  Wipro_28Oct25App.swift
+//  Wipro_28Oct25
+//
+//  Created by Prem Sai K Varma on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Wipro_28Oct25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
